127a6f0c583e05be537ea35598ba18a4edb45ca8
mkoltonowski <mkoltonowski@edu.cdv.pl>
2025-05-04 14:02:14 +0200

    test: model output

--- src/util/transcribePcm.ts
+++ src/util/transcribePcm.ts
@@ -18,7 +18,7 @@ export async function transcribePcm(pcmPath: string): Promise<string> {
 
   const p = spawn(whisperPath, ["-m", modelPath, "-f", wavPath, "-l", "pl"]);
   p.stdout.on("data", (data) => console.log("OUT:", data.toString()));
-  p.stderr.on("data", (data) => console.error("ERR:", data.toString()));
+  // p.stderr.on("data", (data) => console.error("ERR:", data.toString()));
   p.on("close", (code) => console.log("Whisper exited with code", code));
   return whisperPath;
 }
